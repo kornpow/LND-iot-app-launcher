749a10f5611c2d7c15e4edd371bce1f1889ef49d
Sam Korn <korn94sam@gmail.com>
2019-03-13 23:01:44 -0400

    add pip installs

--- code/channelviewer.py
+++ code/channelviewer.py
@@ -1,11 +1,11 @@
-import codecs, grpc, os
 import rpc_pb2 as ln, rpc_pb2_grpc as lnrpc
 import code
+import codecs, grpc, os
 
 
 macaroon = codecs.encode(open('/root/.lnd/data/chain/bitcoin/mainnet/admin.macaroon', 'rb').read(), 'hex')
 os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
-cert = open('/home/pi/.lnd/tls.cert', 'rb').read()
+cert = open('/root/.lnd/tls.cert', 'rb').read()
 ssl_creds = grpc.ssl_channel_credentials(cert)
 channel = grpc.secure_channel('localhost:10009', ssl_creds)
 stub = lnrpc.LightningStub(channel)
