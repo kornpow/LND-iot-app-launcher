a444899ae4e4b61a4ee37c301661a91872e6e8b4
Sam Korn <korn94sam@gmail.com>
2019-03-14 00:38:35 -0400

    latest updates to channel viewer

--- channelviewer.py
+++ channelviewer.py
@@ -1,12 +1,11 @@
+import codecs, grpc, os
 import rpc_pb2 as ln, rpc_pb2_grpc as lnrpc
 import code
-import codecs, grpc, os
-import json
-
+from pprint import pprint
 
 macaroon = codecs.encode(open('/root/.lnd/data/chain/bitcoin/mainnet/admin.macaroon', 'rb').read(), 'hex')
 os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
-cert = open('/root/.lnd/tls.cert', 'rb').read()
+cert = open('/home/pi/.lnd/tls.cert', 'rb').read()
 ssl_creds = grpc.ssl_channel_credentials(cert)
 channel = grpc.secure_channel('localhost:10009', ssl_creds)
 stub = lnrpc.LightningStub(channel)
@@ -15,5 +14,9 @@ stub = lnrpc.LightningStub(channel)
 request = ln.ListChannelsRequest()
 response = stub.ListChannels(request, metadata=[('macaroon', macaroon)])
 
-channelist = json.loads(dict(response) )
 print(response)
+channel_goods = {}
+for chan in response.channels:
+ channel_goods[ str(chan.remote_pubkey) ] = int(chan.local_balance) / ( int(chan.local_balance) + int(chan.remote_balance) )
+
+code.interact(local=locals())
