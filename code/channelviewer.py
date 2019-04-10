1bda3422d9695845a2b8bcb9006412cc35f01c6b
Sam Korn <korn94sam@gmail.com>
2019-04-09 22:39:26 -0400

    comment enhancements

--- code/channelviewer.py
+++ code/channelviewer.py
@@ -15,5 +15,35 @@ stub = lnrpc.LightningStub(channel)
 request = ln.ListChannelsRequest()
 response = stub.ListChannels(request, metadata=[('macaroon', macaroon)])
 
-channelist = json.loads(dict(response) )
+
+for chan in response.channels:
+   request = ln.NodeInfoRequest(pub_key=chan.remote_pubkey)
+   r = stub.GetNodeInfo(request, metadata=[('macaroon', macaroon)])
+   chan_partner_alias = r.node.alias
+   print( "%s\t%s\t%f" % ( chan_partner_alias, chan.remote_pubkey, ( chan.local_balance / ( chan.local_balance+chan.remote_balance ) ) ) )
+
+
+# Query routes for one direction
+request = ln.QueryRoutesRequest(
+   pub_key="027ce055380348d7812d2ae7745701c9f93e70c1adeb2657f053f91df4f2843c71",
+   amt=300000,
+   num_routes=5,
+)
+response = stub.QueryRoutes(request, metadata=[('macaroon', macaroon)])
 print(response)
+topartner = response.routes
+
+request = ln.GetInfoRequest()
+response = stub.GetInfo(request, metadata=[('macaroon', macaroon)])
+print(response)
+mypub = response.identity_pubkey
+
+request = ln.QueryRoutesRequest(
+   pub_key=mypub,
+   amt=300000,
+   num_routes=5,
+   source_pub_key="027ce055380348d7812d2ae7745701c9f93e70c1adeb2657f053f91df4f2843c71",
+)
+response = stub.QueryRoutes(request, metadata=[('macaroon', macaroon)])
+print(response)
+frompartner = response.routes
